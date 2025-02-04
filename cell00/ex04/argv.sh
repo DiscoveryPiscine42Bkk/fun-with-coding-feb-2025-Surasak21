88aa185d03f8572045b1b7b704a5500dec21eb75
Surasak21 <64010934@kmitl.ac.th>
2025-02-04 14:38:22 +0700

    ex04

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,20 @@
+if [ $# -eq 0 ]
+then
+    echo "no arguments"
+elif [ $# -eq 1 ]
+then
+    echo "$1"
+elif [ $# -eq 2 ]
+then
+    echo "$1"
+    echo "$2"
+elif [ $# -eq 3 ]
+then
+    echo "$1"
+    echo "$2"
+    echo "$3"
+else
+    echo "$1"
+    echo "$2"
+    echo "$3"
+fi
